764f1c49bbef8bb3fd4f4344002421793f6d653b
rsarles-sensonics <123006892+rsarles-sensonics@users.noreply.github.com>
2023-01-18 12:11:19 -0500

    Add files via upload

--- sUtils.py
+++ sUtils.py
@@ -103,3 +103,49 @@ def showAnswers(img,myIndex,grading,ans,questions,choices):
             cv2.circle(img,(centerX,centerY),20,(0,0,255),cv2.FILLED) #if incorrect fill red
             cv2.circle(img,(centerX,((ans[x]*secHeight) + (secHeight)//2) + 130),20,(255,0,0),5) #outline correct in blue
     return img
+
+
+    #"
+    #  REVISED UPSIT ITEMS ARE AS FOLLOWS. CORRECT ITEMS ARE IN CAPS
+    #  1. a. honey           b. PIZZA              c. orange             d. bubble gum
+    #  2. a. leather         b. BUBBLE GUM         c. chili              d. dill pickle
+    #  3. a. tomato          b. banana             c. strawberry         d. MENTHOL/EUCALYPTUS
+    #  4. a. whiskey         b. honey              c. lime               d. CHERRY
+    #  5. a. chocolate       b. pizza              c. MOTOR OIL          d. cheddar cheese
+    #  6. a. beer            b. MINT               c. grape candy        d. cola
+    #  7. a. BANANA          b. garlic             c. rubber tire        d. clove
+    #  8. a. peanut          b. CLOVE              c. lemon              d. banana
+    #  9. a. clove           b. baby powder        c. LEATHER            d. apple
+    # 10. a. wood            b. COCONUT            c. smoke              d. honey
+    # 11. a. wintergreen gum b. grape candy        c. ONION              d. tomato
+    # 12. a. pine            b. RASPBERRY          c. menthol/eucalyptus d. beer
+    # 13. a. LICORICE/ANISE  b. sandalwood/incense c. coconut            d. raspberry
+    # 14. a. paint thinner   b. cherry             c. coconut            d. BABY POWDER
+    # 15. a. baby powder     b. CINNAMON           c. tomato             d. coconut
+    # 16. a. coconut         b. fish               c. banana             d. GASOLINE
+    # 17. a. STRAWBERRY      b. gasoline           c. dill pickle        d. wood
+    # 18. a. CEDAR           b. cheddar cheese     c. lemon              d. vanilla
+    # 19. a. lemon           b. CHOCOLATE          c. garlic             d. black pepper
+    # 20. a. coffee          b. soap               c. WINTEGREEN GUM     d. garlic
+    # 21. a. FLOWER          b. rubber tire        c. chocolate          d. fish
+    # 22. a. COTTON CANDY    b. onion              c. natural gas        d. chili
+    # 23. a. fish            b. PEACH              c. leather            d. pizza
+    # 24. a. GARLIC          b. watermelon         c. licorice/anise     d. smoke
+    # 25. a. pineapple       b. DILL PICKLE        c. apple              d. chocolate
+    # 26. a. smoke           b. popcorn            c. PINEAPPLE          d. onion
+    # 27. a. vanilla         b. garlic             c. peach              d. RUBBER TIRE
+    # 28. a. rubber tire     b. ORANGE             c. cheddar cheese     d. tobacco
+    # 29. a. mint            b. APPLE              c. fish               d. leather
+    # 30. a. coffee          b. menthol/eucalyptus c. peanut             d. WATERMELON
+    # 31. a. watermelon      b. peanut             c. beer               d. SANDALWOOD/INCENSE
+    # 32. a. apple           b. beer               c. GRASS              d. coffee
+    # 33. a. dill pickle     b. grass              c. SMOKE              d. peach
+    # 34. a. PINE            b. tomato             c. onion              d. popcorn
+    # 35. a. pizza           b. cigar              c. peanut             d. GRAPE CANDY
+    # 36. a. tomato          b. smoke              c. rose               d. LEMON
+    # 37. a. SOAP            b. chocolate          c. pizza              d. peanut
+    # 38. a. orange          b. wintergreen gum    c. cola               d. NATURAL GAS
+    # 39. a. lime            b. ROSE               c. fish               d. mint
+    # 40. a. PEANUT          b. lemon              c. cinnamon           d. natural gas
+    #
+    # " 
